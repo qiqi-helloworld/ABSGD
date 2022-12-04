cfc1df41a4dfc386e34558dabbad45a6baff04b8
QiQi <qi-qi@uiowa.edu>
2022-12-04 01:00:12 -0600

    Add files via upload

--- toy-example.py
+++ toy-example.py
@@ -73,7 +73,9 @@ def grad_logistic(x, y, pred, p):
 
 
 def grad_DRO(bth_x, bth_y, pred, lamda = 10.0):
-
+    '''
+    gradients of DRO
+    '''
     loss = logisticLoss(bth_y, pred)
     max_loss = np.max(loss)
     exp_loss = np.exp((loss - max_loss) / lamda)
@@ -82,6 +84,9 @@ def grad_DRO(bth_x, bth_y, pred, lamda = 10.0):
 
 
 def grad_Reweight(bth_x, bth_y, pred, beta):
+    '''
+    gradients of class balanced reweighting loss + DRO
+    '''
     cls_num_list = [100, num_minor_sample]
     effective_num = 1.0 - np.power(beta, cls_num_list)
     per_cls_weights = (1.0 - beta) / np.array(effective_num)
@@ -95,23 +100,32 @@ def grad_Reweight(bth_x, bth_y, pred, beta):
 
 
 def grad_RE_DRO(bth_x, bth_y, pred, lamda, beta):
-     cls_num_list = [100, num_minor_sample]
-     effective_num = 1.0 - np.power(beta, cls_num_list)
-     per_cls_weights = (1.0 - beta) / np.array(effective_num)
-     per_cls_weights = per_cls_weights / np.sum(per_cls_weights)
+    '''
+    gradients of class balanced reweighting loss + DRO
+    :param bth_x: batch x
+    :param bth_y: batch x
+    :param pred: preductions
+    :param lamda: temperature
+    :param beta: class weights parameter
+    :return:
+    '''
+    cls_num_list = [100, num_minor_sample]
+    effective_num = 1.0 - np.power(beta, cls_num_list)
+    per_cls_weights = (1.0 - beta) / np.array(effective_num)
+    per_cls_weights = per_cls_weights / np.sum(per_cls_weights)
 
-     new_y = copy.deepcopy(bth_y)
-     new_y[new_y == -1] = 0
-     p_RE = per_cls_weights[new_y]
-     p_RE = p_RE / np.sum(p_RE)
-     loss = logisticLoss(bth_y, pred)
-     loss = p_RE * loss
+    new_y = copy.deepcopy(bth_y)
+    new_y[new_y == -1] = 0
+    p_RE = per_cls_weights[new_y]
+    p_RE = p_RE / np.sum(p_RE)
+    loss = logisticLoss(bth_y, pred)
+    loss = p_RE * loss
 
-     max_loss = np.max(loss)
-     exp_loss = np.exp((loss - max_loss) / lamda)
-     p = exp_loss / np.sum(exp_loss)
+    max_loss = np.max(loss)
+    exp_loss = np.exp((loss - max_loss) / lamda)
+    p = exp_loss / np.sum(exp_loss)
 
-     return grad_logistic(bth_x, bth_y, pred, p)
+    return grad_logistic(bth_x, bth_y, pred, p)
 
 def train_toyexample(w, methods, bth, beta, lamda = 0.2):
 
@@ -183,8 +197,7 @@ def train_toyexample(w, methods, bth, beta, lamda = 0.2):
 
 def ABSGD_sample_weights_plot(loss, lamda):
     '''
-
-    :param sample: sample losses used for calculate ABSGD weights
+    :param sample: sample losses used for calculate absgd weights
     :param lamda:  hyper parameters to calculate weights
     :return: figures of weights plots
     '''
@@ -200,10 +213,10 @@ def ABSGD_sample_weights_plot(loss, lamda):
 
 
     plt.figure(figsize=(5,5))
-    plt.bar(np.array(range(16))+0.875, p, width = 0.25,color = 'red', label='ABSGD')
+    plt.bar(np.array(range(16))+0.875, p, width = 0.25,color = 'red', label='absgd')
     plt.bar(np.array(range(16))+1.125, sgd_weights, width = 0.25, color = 'blue', label='SGD')
     # np.array(range(len(loss))) + 0.5, np.array(range(len(loss)))+1,
-    # plt.hist(p,  16, color = 'red', label='ABSGD')
+    # plt.hist(p,  16, color = 'red', label='absgd')
     # plt.hist(sgd_weights, 16,  color = 'blue', label='SGD')
     plt.ylabel(r' $p$', fontsize = 15)
     plt.xlabel(r'Sample Index', fontsize=15)
@@ -213,131 +226,131 @@ def ABSGD_sample_weights_plot(loss, lamda):
 
     plt.show()
 
-"""
-Load Data
-"""
-if os.path.exists('toy_example.csv'):
-    data = pd.read_csv('toy_example.csv')
-    data = data/100
-    minor_x = data['minor_x']
-    minor_y = data['minor_y']
-    majority_x = data['majority_x']
-    majority_y = data['majority_y']
-else:
-    mu = 60
-    sigma = 5
-    minor_x = []
-    minor_y = []
-    majority_x = []
-    majority_y = []
-    for i in range(100):
-        if i <= num_minor_sample:
-            tempX = random.gauss(mu, sigma)
-            minor_x.append(tempX)
-            tempY = random.gauss(mu, sigma)
-            minor_y.append(tempY)
-        else:
-            minor_x.append(-1)
-            minor_y.append(-1)
-
-    for i in range(100):
-        tempX = random.gauss(mu-num_minor_sample, sigma+5)
-        majority_x.append(tempX)
-        tempY = random.gauss(mu-num_minor_sample, sigma+5)
-        majority_y.append(tempY)
 
 
 
+if __name__ == '__main__':
 
-pos_samples = np.array(data[['minor_x', 'minor_y']])[0:num_minor_sample]
-pos_target = np.array([[1]]*num_minor_sample)
-neg_samples = np.array(data[['majority_x', 'majority_y']])
-neg_target = np.array([[-1]]*num_majority_sample)
-
-#print(len(pos_samples))
-x_train = np.vstack((pos_samples, neg_samples))
-y_train = np.vstack((pos_target, neg_target))
-
-
-x_train = np.hstack((x_train, np.array([[1]]*(num_majority_sample+num_minor_sample))))
-
-
-# w_0 = np.array([-1, -1, 1.15])
-#w_0 = np.array([0,0,0])
-# w_0 = np.array([10.23435738, 10.83358531, -12.37028814])
-# w_0 = np.array([7.33091949, 7.16490501, -8.79802186])
-w_0 = np.array([5.99308785, 6.13973099, -8.03277303])
-w_0 = w_0.reshape((3,1))
-# w = copy.deepcopy(w_0)
-
-x = np.array(range(80))/100
-maxIter = 100000
-bth_list = [16, 32, 64, 100 + num_minor_sample]
-
-lamda_beta_list = [0, 0, 0.9, 0.95, 0.99, 0.999, 0.01, 0.05, 0.1, 0.5, 1, 5]
-
-random.seed(777)
-ada_beta = [0.05, 0.2, 0.1, 0.05, 0.03]
-title =['SGD', 'Stochastic Robust Weighting']
-methods = ['ERM', 'DRO' ]
-lbd = 0.05
-beta = 0.999
-bth =128
-eta = 0.1
-
-index_ls = np.random.choice(range(num_majority_sample + num_minor_sample), size=maxIter*bth, replace=True, p=None)
-index_ls = [46, 107, 11, 119, 1, 32, 68, 4, 110, 83, 115, 62, 56, 9, 14, 100] + index_ls.tolist()
-
-
-indexed_samples_for_ABSGD = [46, 107, 11, 119, 1, 32, 68, 4, 110, 83, 115, 62, 56, 9, 14, 100]
-# index_ls = [23, 35, 11, 119, 1, 32, 68, 4, 35, 83, 104, 118, 56, 9, 14, 21]
-
-
-for i in range(1, len(title)):
-    if i == 1:
-        lbd = 0.2
+    """
+    Load Data
+    """
+    if os.path.exists('toy_example.csv'):
+        data = pd.read_csv('toy_example.csv')
+        data = data/100
+        minor_x = data['minor_x']
+        minor_y = data['minor_y']
+        majority_x = data['majority_x']
+        majority_y = data['majority_y']
     else:
-        lbd = 0.1
-    if i == 3:
-        beta = 0.99
-    w = copy.deepcopy(w_0)
-    # w1 = w
-    w1, loss = train_toyexample(w, methods[i], 16, beta, lbd)
+        mu = 60
+        sigma = 5
+        minor_x = []
+        minor_y = []
+        majority_x = []
+        majority_y = []
+        for i in range(100):
+            if i <= num_minor_sample:
+                tempX = random.gauss(mu, sigma)
+                minor_x.append(tempX)
+                tempY = random.gauss(mu, sigma)
+                minor_y.append(tempY)
+            else:
+                minor_x.append(-1)
+                minor_y.append(-1)
 
-    pred = np.matmul(x_train[indexed_samples_for_ABSGD], w)
-    i_loss = logisticLoss(y_train[indexed_samples_for_ABSGD], pred)
-
-    ABSGD_sample_weights_plot(i_loss, lbd)
-    # print(title[i], "weights:", w1)
-
-    # ax = fig.add_subplot(gs[i//2, i%2])
-    # plt.plot(loss)
-    # plt.show()
-
-    # plt.plot(x, -w1[0] / w1[1] * x - w1[2] / w1[0], '-', color='black', linewidth=3, label='DRO')
-    # plt.fill_between(x, -w1[0] / w1[1] * x - w1[2] / w1[0], interpolate=True, color='antiquewhite') # antiquewhite
-    # plt.fill_between(x, -w1[0] / w1[1] * x - w1[2] / w1[0], 100, interpolate=True, color='lightcyan') # lightcyan
-    #
-    # plt.plot(minor_x[0:num_minor_sample], minor_y[0:num_minor_sample], 'o', color = 'yellowgreen') #salmon
-    # plt.scatter(minor_x[0:num_minor_sample], minor_y[0:num_minor_sample], color = 'green', linewidth=4)
-    # plt.plot(majority_x, majority_y,'o', color = 'royalblue')
-    # plt.scatter(majority_x, majority_y, color='blue', linewidth=3)
-    #
-    # # # print(x_train[index_ls][:,0], y_train[index_ls])
-    # plt.scatter(x_train[index_ls][:,0], x_train[index_ls][:,1], color = 'red', s = 100 )
-    # plt.xticks([])
-    # plt.yticks([])
-    # plt.xlim(min(majority_x) - 0.05, max(minor_x)+0.05)
-    # plt.ylim(min(majority_y) - 0.05, max(minor_y)+0.05)
-    # plt.title(title[i], fontsize = 25)
-    # plt.savefig(title[i] + '.png')
-    # print('--i---', i)
-    #plt.savefig("init" + '.png')
-    # plt.show()
+        for i in range(100):
+            tempX = random.gauss(mu-num_minor_sample, sigma+5)
+            majority_x.append(tempX)
+            tempY = random.gauss(mu-num_minor_sample, sigma+5)
+            majority_y.append(tempY)
 
 
-# if __name__ == '__main__':x_train
-#    pass
- # fig_lambda_p(w_0, x_train[index_ls], y_train[index_ls])
+
+
+    pos_samples = np.array(data[['minor_x', 'minor_y']])[0:num_minor_sample]
+    pos_target = np.array([[1]]*num_minor_sample)
+    neg_samples = np.array(data[['majority_x', 'majority_y']])
+    neg_target = np.array([[-1]]*num_majority_sample)
+    x_train = np.vstack((pos_samples, neg_samples))
+    y_train = np.vstack((pos_target, neg_target))
+    x_train = np.hstack((x_train, np.array([[1]]*(num_majority_sample+num_minor_sample))))
+    # w_0 = np.array([-1, -1, 1.15])
+    #w_0 = np.array([0,0,0])
+    # w_0 = np.array([10.23435738, 10.83358531, -12.37028814])
+    # w_0 = np.array([7.33091949, 7.16490501, -8.79802186])
+    '''
+    Initialization
+    '''
+    w_0 = np.array([5.99308785, 6.13973099, -8.03277303])
+    w_0 = w_0.reshape((3,1))
+    # w = copy.deepcopy(w_0)
+
+    '''
+       Hyperparameters
+    '''
+    maxIter = 100000
+    bth_list = [16, 32, 64, 100 + num_minor_sample]
+    lamda_beta_list = [0, 0, 0.9, 0.95, 0.99, 0.999, 0.01, 0.05, 0.1, 0.5, 1, 5]
+    random.seed(777)
+    ada_beta = [0.05, 0.2, 0.1, 0.05, 0.03]
+    title =['SGD', 'Stochastic Robust Weighting']
+    methods = ['ERM', 'DRO' ]
+    lbd = 0.05
+    beta = 0.999
+    bth =128
+    eta = 0.1
+
+    index_ls = np.random.choice(range(num_majority_sample + num_minor_sample), size=maxIter*bth, replace=True, p=None)
+    index_ls = [46, 107, 11, 119, 1, 32, 68, 4, 110, 83, 115, 62, 56, 9, 14, 100] + index_ls.tolist()
+    indexed_samples_for_ABSGD = [46, 107, 11, 119, 1, 32, 68, 4, 110, 83, 115, 62, 56, 9, 14, 100]
+    # index_ls = [23, 35, 11, 119, 1, 32, 68, 4, 35, 83, 104, 118, 56, 9, 14, 21]
+
+
+    for i in range(1, len(title)):
+        if i == 1:
+            lbd = 0.2
+        else:
+            lbd = 0.1
+        if i == 3:
+            beta = 0.99
+        w = copy.deepcopy(w_0)
+        # w1 = w
+        w1, loss = train_toyexample(w, methods[i], 16, beta, lbd)
+
+        pred = np.matmul(x_train[indexed_samples_for_ABSGD], w)
+        i_loss = logisticLoss(y_train[indexed_samples_for_ABSGD], pred)
+
+        ABSGD_sample_weights_plot(i_loss, lbd)
+        # print(title[i], "weights:", w1)
+
+        # ax = fig.add_subplot(gs[i//2, i%2])
+        # plt.plot(loss)
+        # plt.show()
+
+        # plt.plot(x, -w1[0] / w1[1] * x - w1[2] / w1[0], '-', color='black', linewidth=3, label='DRO')
+        # plt.fill_between(x, -w1[0] / w1[1] * x - w1[2] / w1[0], interpolate=True, color='antiquewhite') # antiquewhite
+        # plt.fill_between(x, -w1[0] / w1[1] * x - w1[2] / w1[0], 100, interpolate=True, color='lightcyan') # lightcyan
+        #
+        # plt.plot(minor_x[0:num_minor_sample], minor_y[0:num_minor_sample], 'o', color = 'yellowgreen') #salmon
+        # plt.scatter(minor_x[0:num_minor_sample], minor_y[0:num_minor_sample], color = 'green', linewidth=4)
+        # plt.plot(majority_x, majority_y,'o', color = 'royalblue')
+        # plt.scatter(majority_x, majority_y, color='blue', linewidth=3)
+        #
+        # # # print(x_train[index_ls][:,0], y_train[index_ls])
+        # plt.scatter(x_train[index_ls][:,0], x_train[index_ls][:,1], color = 'red', s = 100 )
+        # plt.xticks([])
+        # plt.yticks([])
+        # plt.xlim(min(majority_x) - 0.05, max(minor_x)+0.05)
+        # plt.ylim(min(majority_y) - 0.05, max(minor_y)+0.05)
+        # plt.title(title[i], fontsize = 25)
+        # plt.savefig(title[i] + '.png')
+        # print('--i---', i)
+        #plt.savefig("init" + '.png')
+        # plt.show()
+
+
+    # if __name__ == '__main__':x_train
+    #    pass
+     # fig_lambda_p(w_0, x_train[index_ls], y_train[index_ls])
 
 
